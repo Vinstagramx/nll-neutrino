fixed contour plots. gradient method still needs work

main.py
@@ -212,7 +212,7 @@ data = [en_array, event_no, exp_data]  # Data to be passed into the Minimise1D o
 # """
 # Section 4 - Preliminary Investigations
 # - Using values around the minimising value of theta, the behaviour of the NLL with varying squared mass difference is then investigated.
-# - Also plotted a contour plot to further investigate the behaviour.
+# - Also created contour plots to further investigate the behaviour.
 # """
 # num_m = 500  # Number of squared mass differences to investigate
 # m_arr = np.linspace(1e-4, 1e-2, num_m)  # Generating an array of squared mass differences from [10^-4,10^-2]
@@ -242,35 +242,35 @@ data = [en_array, event_no, exp_data]  # Data to be passed into the Minimise1D o
   
 #   plot_counter += 1  # Increments counter by 1
 
-# # Plotting a contour plot
+# # Creating contour plots
 # x_theta = np.linspace(0.3, 1.2, 50)  # Array of mixing angles
 # y_mass = np.linspace(5e-4, 5e-3, 50)  # Array of squared mass differences
-# X, Y = np.meshgrid(x_theta, y_mass)  
+# X, Y = np.meshgrid(x_theta, y_mass)  # Forms two 'grids' of X and Y data
 # z_nll = np.empty((50,50))  # Empty 2-D array initialised for the NLL values
-# # Calculating 500 x 500 NLL values for the contour plot
+# # Calculating 50 x 50 NLL values for the contour plot
 # for i in range(0, len(x_theta)):
 #   for j in range(0, len(y_mass)):
-#     nll_contour = NLL(en_array, event_no, exp_data, mix_ang = x_theta[i], distance = L, sq_mass_diff = y_mass[j])
+#     nll_contour = NLL(en_array, event_no, exp_data, mix_ang = X[i][j], distance = L, sq_mass_diff = Y[i][j])
 #     nll_contour.surv_prob()
 #     nll_contour.calc_lambda()
 #     z_nll[i][j] = nll_contour.find_nll()  # Calculating the NLL
 # # Contour line plot
-# plots.contour(X, Y, z_nll, filename = "contour", colorbar = True, fill = False, \
+# plots.contour(X, Y, z_nll, filename = "contour", colorbar = False, fill = False, \
 #               title = "Contour Plot of NLL vs Mixing Angle and squared mass difference", xlabel = r"$\theta_{23}$ (rads)", ylabel = r"$\Delta_{23}^2$ (eV$^2$)")
 # # Filled contour plot
 # plots.contour(X, Y, z_nll, filename = "contour_fill", colorbar = True, fill = True, \
 #               title = "Contour Plot of NLL vs Mixing Angle and squared mass difference", xlabel = r"$\theta_{23}$ (rads)", ylabel = r"$\Delta_{23}^2$ (eV$^2$)")
 
-# """
-# Section 4.1 - Univariate 2-D minimisation
-# - Note: x refers to the mixing angle, and y refers to the squared mass difference.
-# """
-# data = [en_array, event_no, exp_data]  # Data to be passed into the Minimise2D object
-# # Creating a Minimise2D object for univariate minimisation
-# min_2d = Minimise2D([0.55, 0.78], [1e-3, 4e-3], nll = True, nll_data = data)
+"""
+Section 4.1 - Univariate 2-D Minimisation
+- Note: x refers to the mixing angle, and y refers to the squared mass difference.
+"""
+data = [en_array, event_no, exp_data]  # Data to be passed into the Minimise2D object
+# Creating a Minimise2D object for univariate minimisation
+min_2d = Minimise2D([0.55, 0.78], [1e-3, 4e-3], nll = True, nll_data = data)
 # # Minimising the mixing angle (x-direction) first
 # min_2d.univ_min(first = 'x')
-# print("--- 2-D Minimisation (Mixing Angle first) ---")
+# print("--- 2-D Univariate Minimisation (Mixing Angle first) ---")
 # print(f"Mixing Angle which minimises NLL: {min_2d.min[0]}")
 # print(f"Squared Mass Difference which minimises NLL: {min_2d.min[1]}")
 # print(f"NLL value: {min_2d.dir_min_func}")
@@ -280,10 +280,21 @@ data = [en_array, event_no, exp_data]  # Data to be passed into the Minimise1D o
 
 # # Minimising the squared mass difference (y-direction) first
 # min_2d.univ_min(first = 'y')
-# print("--- 2-D Minimisation (Squared Mass Diff first)---")
+# print("--- 2-D Univariate Minimisation (Squared Mass Diff first)---")
 # print(f"Mixing Angle which minimises NLL: {min_2d.min[0]}")
 # print(f"Squared Mass Difference which minimises NLL: {min_2d.min[1]}")
 # print(f"NLL value: {min_2d.dir_min_func}")
 # print(f"Total iterations: {min_2d.iterations}")
 # print(f"x-direction --> Iterations: {min_2d.x_iters}, Minimisations: {min_2d.min_iters_x}")
 # print(f"y-direction --> Iterations: {min_2d.y_iters}, Minimisations: {min_2d.min_iters_y}")
+
+"""
+Section 4.2 - Testing Simultaneous Minimisation Schemes
+"""
+# Gradient scheme
+min_2d.grad_min(alpha = 1e-7)
+print("--- 2-D Simultaneous Minimisation (Gradient Method) ---")
+print(f"Mixing Angle which minimises NLL: {min_2d.min[0]}")
+print(f"Squared Mass Difference which minimises NLL: {min_2d.min[1]}")
+print(f"NLL value: {min_2d.nll_min}")
+print(f"Total iterations: {min_2d.iterations}")

minim_2d.py
@@ -264,23 +264,54 @@ class Minimise2D():
         """Generates a starting point for iteration of the 2-D minimisation schemes (excluding univariate).
 
         Picks a random coordinate from the x- and y- initialisation ranges used, and saves this coordinate as 
-        a private member variable, in the form of a tuple.
+        a private member variable, in the form of a NumPy array.
+
+        Returns:
+            coord: Starting coordinate for the given minimisation scheme.
         """
         x_init = np.random.uniform(self._init_range_x[0], self._init_range_x[1])
         y_init = np.random.uniform(self._init_range_y[0], self._init_range_y[1])
-        self._coord = (x_init, y_init)
+        coord = np.array([x_init, y_init])
+        return coord
 
     def grad_min(self, alpha):
         """Gradient minimisation method for 2 dimensions.
 
         Follows the steepest descent in gradient towards the minimum. This is done by calculating the gradient using a forward
         difference scheme, and taking a small step α in the direction opposite the gradient (as the gradient is perpendicular to the local contour line).
+        However, α was scaled so that it has an equivalent relative magnitude in both coordinate directions, for optimal efficiency. 
         The coordinate is updated with each step taken, and iterations occur until the convergence condition is satisfied.
         """
+        self._iterations = 0
+        self._minimum_found = False
+        self._prev_coord = self.gen_start_pt()  # Generating starting position
+        scaling = np.mean(self._init_range_x) / np.mean(self._init_range_y)  # Scaling factor
+        print(scaling)
+        # As the mass is smaller in size than the mixing angle, we scale alpha to have the same relative size for both parameters.
+        alpha_x = alpha
+        alpha_y = alpha_x / scaling 
+        while not self._minimum_found:
+            d = np.empty(2)  # Gradient vector
+            d[0] = (self.calc_nll(self._prev_coord[0] + alpha_x, self._prev_coord[1]) - self.calc_nll(self._prev_coord[0], self._prev_coord[1])) / alpha_x
+            d[1] = (self.calc_nll(self._prev_coord[0], self._prev_coord[1] + alpha_y) - self.calc_nll(self._prev_coord[0], self._prev_coord[1])) / alpha_y
+            new_coord = self._prev_coord - (alpha * d)
+            if self._iterations == 0:
+                self._prev_coord = new_coord
+            else:
+                rel_diff_x = abs(self._prev_coord[0] - new_coord[0]) / self._prev_coord[0]
+                rel_diff_y = abs(self._prev_coord[1] - new_coord[1]) / self._prev_coord[1]
+                if rel_diff_x < 1e-5 and rel_diff_y < 1e-5:
+                    self._minimum_found = True
+                    self._min = new_coord
+                    self._nll_min = self.calc_nll(new_coord[0], new_coord[1])
+                else:
+                    self._prev_coord = new_coord
+                    print(new_coord, 'iterations:' , self._iterations)
+            
+            self._iterations += 1
+        
+        return self._min
 
-        d = np.empty(2)  # Gradient vector
-        d[0] = (self.calc_nll(self._coord[0] + alpha, self._coord[1]) - self.calc_nll(self._coord[0], self._coord[1]) / alpha)
-        d[1] = (self.calc_nll(self._coord[0], self._coord[1] + alpha) - self.calc_nll(self._coord[0], self._coord[1]) / alpha)
 
     """
     Getters to access the private member variables outside the class.
@@ -297,6 +328,10 @@ class Minimise2D():
     @property
     def dir_min_func(self):
         return self._dir_min_func
+    
+    @property
+    def nll_min(self):
+        return self._nll_min
     
     @property
     def min_iters_x(self):